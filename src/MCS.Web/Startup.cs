68cd96af4d1f9645f4d7c5d2e7b59e80bf78862f
MiZone <mizonerom@hotmail.com>
2020-06-17 17:40:19 +0800

    -

--- src/MCS.Web/Startup.cs
+++ src/MCS.Web/Startup.cs
@@ -73,6 +73,7 @@ namespace MCS.Web
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
 
             app.UseRouting();
