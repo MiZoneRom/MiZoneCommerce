93f320415367ce6b004b8bafd3b2c052e1abf4e6
MiZone <mizonerom@hotmail.com>
2020-06-17 17:31:11 +0800

    -

---
+++ src/MCS.Web/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MCS.Web.Controllers
+{
+    /// <summary>
+    /// 测试接口
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        /// <summary>
+        /// 获取信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+
+            //var services = new ServiceCollection();
+            //var provider = services.BuildServiceProvider();
+            //services.AddSingleton<IManagerService, Operation>();
+            //// 自定义传入Guid空值
+            //services.AddSingleton<IManagerService>(
+            //  new Operation(Guid.Empty));
+            //// 自定义传入一个New的Guid
+            //services.AddSingleton<IManagerService>(
+            //  new Operation(Guid.NewGuid()));
+            //provider.GetService<IManagerService>().GetManagers("aaa");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- src/MCS.Web/Startup.cs
+++ src/MCS.Web/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Dapper.Common;
+using Microsoft.OpenApi.Models;
+using System.IO;
 
 namespace MCS.Web
 {
@@ -24,6 +26,14 @@ namespace MCS.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+                var xmlPath = Path.Combine(basePath, "MCS.Web.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +47,15 @@ namespace MCS.Web
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
             app.UseStaticFiles();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
