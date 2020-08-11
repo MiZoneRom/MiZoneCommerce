afe7eecff715ea96ee849d1faac89ddebd719849
MiZone <mizonerom@hotmail.com>
2020-08-11 14:09:06 +0800

    -

--- src/MCS.Web.Framework/PrivilegeConfig/PrivilegeHelper.cs
+++ src/MCS.Web.Framework/PrivilegeConfig/PrivilegeHelper.cs
@@ -97,87 +97,7 @@ namespace MCS.Web.Framework
         /// <returns></returns>
         public static Privileges GetPrivileges<TEnum>()
         {
-            SiteSettings sitesetting = SiteSettingApplication.SiteSettings;
-            Type type = typeof(TEnum);
-            FieldInfo[] fields = type.GetFields();
-            if (fields.Length == 1)
-            {
-                return null;
-            }
-            Privileges p = new Privileges();
-            foreach (var field in fields)
-            {
-                var privilegeAttributes = field.GetCustomAttributes(typeof(PrivilegeAttribute), true);
-
-                if (privilegeAttributes.Length == 0)
-                {
-                    continue;
-                }
-
-                GroupActionItem group = new GroupActionItem();
-                ActionItem item = new ActionItem();
-                List<PrivilegeAttribute> privilegeAttributeList = new List<PrivilegeAttribute>();
-                List<Controllers> ctrls = new List<Controllers>();
-
-                foreach (var attr in privilegeAttributes)
-                {
-                    Controllers ctrl = new Controllers();
-                    var attribute = attr as PrivilegeAttribute;
-                    ctrl.ControllerName = attribute.Controller;
-                    ctrl.ActionNames.AddRange(attribute.Action.Split(','));
-                    ctrls.Add(ctrl);
-                    privilegeAttributeList.Add(attribute);
-                }
-
-                //var privilegeAttributeGroupInfo = privilegeAttributeList.FirstOrDefault(a => !string.IsNullOrEmpty(a.GroupName));
-                //if (sitesetting != null)
-                //{
-
-                //}
-
-                //group.Name = privilegeAttributeGroupInfo.GroupName;
-
-                //item.PrivilegeId = privilegeAttributeGroupInfo.Pid;
-                //item.Name = privilegeAttributeGroupInfo.Name;
-                //item.Path = privilegeAttributeGroupInfo.Url;
-                item.Controllers.AddRange(ctrls);
-
-                //获取导航属性
-                var adminNavigations = field.GetCustomAttributes(typeof(AdminNavigationAttribute), true);
-
-                if (adminNavigations.Count() > 0)
-                {
-                    List<AdminNavigationAttribute> adminNavigationAttributeList = new List<AdminNavigationAttribute>();
-                    foreach (var attr in adminNavigations)
-                    {
-                        var attribute = attr as AdminNavigationAttribute;
-                        adminNavigationAttributeList.Add(attribute);
-                    }
-
-                    var adminNavigationGroupInfo = adminNavigationAttributeList.FirstOrDefault(a => !string.IsNullOrEmpty(a.Component));
-
-                    item.IconCls = adminNavigationGroupInfo.IconCls;
-                    item.Type = adminNavigationGroupInfo.AdminCatalogType;
-                    item.LinkTarget = adminNavigationGroupInfo.LinkTarget;
-                    item.Component = adminNavigationGroupInfo.Component;
-
-                }
-
-                var currentGroup = p.Privilege.FirstOrDefault(a => a.Name == group.Name);
-
-                if (currentGroup == null)
-                {
-                    group.Children.Add(item);
-                    group.IconCls = item.IconCls;
-                    p.Privilege.Add(group);
-                }
-                else
-                {
-                    currentGroup.Children.Add(item);
-                }
-            }
-
-            return p;
+            return GetPrivileges<TEnum>(null);
         }
 
         /// <summary>
@@ -186,7 +106,7 @@ namespace MCS.Web.Framework
         /// <param name="path"></param>
         /// <param name="Type">导航类别</param>
         /// <returns></returns>
-        public static Privileges GetPrivileges<TEnum>(AdminCatalogType Type)
+        public static Privileges GetPrivileges<TEnum>(AdminCatalogType? Type)
         {
             SiteSettings sitesetting = SiteSettingApplication.SiteSettings;
             Type type = typeof(TEnum);
