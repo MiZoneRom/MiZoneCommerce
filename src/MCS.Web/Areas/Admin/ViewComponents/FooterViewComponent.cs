a08f27cb4d2d29cd6f473033ca7b6141a77eb04d
MiZone <mizonerom@hotmail.com>
2020-11-12 13:51:09 +0800

    -

---
+++ src/MCS.Web/Areas/Admin/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCS.Web.Areas.Admin.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public FooterViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
