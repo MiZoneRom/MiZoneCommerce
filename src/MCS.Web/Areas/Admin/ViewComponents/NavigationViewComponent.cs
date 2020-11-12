66d01238e471da848444101579db849b8ff1839c
MiZone <mizonerom@hotmail.com>
2020-11-12 11:18:39 +0800

    -

---
+++ src/MCS.Web/Areas/Admin/ViewComponents/NavigationViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCS.Web.Areas.Admin.ViewComponents
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        public NavigationViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
