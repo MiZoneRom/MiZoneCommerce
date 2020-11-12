88021099f1af5d649d0275d4dd6c95946477d894
MiZone <mizonerom@hotmail.com>
2020-11-12 16:09:51 +0800

    -

--- src/MCS.Web/Areas/Admin/Controllers/HomeController.cs
+++ src/MCS.Web/Areas/Admin/Controllers/HomeController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MCS.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize("AccessControl")]
     public class HomeController : Controller
     {
         public IActionResult Index()
