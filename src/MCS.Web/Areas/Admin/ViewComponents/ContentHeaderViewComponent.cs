220b1eb6ca7d7803ac2644b21a0ced9d875e6596
MiZone <mizonerom@hotmail.com>
2020-12-02 13:39:31 +0800

    -

---
+++ src/MCS.Web/Areas/Admin/ViewComponents/ContentHeaderViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using MCS.Application;
+using MCS.Core;
+using MCS.Web.Framework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCS.Web.Areas.Admin.ViewComponents
+{
+    public class ContentHeaderViewComponent : BaseViewComponents
+    {
+        public ContentHeaderViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string path = this.HttpContext.Request.Path;
+            return View();
+        }
+    }
+}
