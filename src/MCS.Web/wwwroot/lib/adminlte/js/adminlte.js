f66366f1fc3cfd1fc7cb0d0d6cd943c74fe3c0b8
MiZone <mizonerom@hotmail.com>
2020-12-02 15:56:59 +0800

    -

--- src/MCS.Web/wwwroot/lib/adminlte/js/adminlte.js
+++ src/MCS.Web/wwwroot/lib/adminlte/js/adminlte.js
@@ -2205,8 +2205,7 @@
     var _proto = Treeview.prototype;
 
     _proto.init = function init() {
-      $__default['default']("" + SELECTOR_LI + SELECTOR_OPEN + " " + SELECTOR_TREEVIEW_MENU).css('display', 'block');
-
+      //$__default['default']("" + SELECTOR_LI + SELECTOR_OPEN + " " + SELECTOR_TREEVIEW_MENU).css('display', 'block');//≥ı ºªØ”∞œÏ
       this._setupListeners();
     };
 
