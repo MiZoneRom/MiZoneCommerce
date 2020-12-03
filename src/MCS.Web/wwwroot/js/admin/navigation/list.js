7b203bbed19402c74e94d4b4c04f89a4401465fe
MiZone <mizonerom@hotmail.com>
2020-12-03 11:41:10 +0800

    -

--- src/MCS.Web/wwwroot/js/admin/navigation/list.js
+++ src/MCS.Web/wwwroot/js/admin/navigation/list.js
@@ -1,21 +1,54 @@
-﻿$(function () {
+﻿var _navList = [];
+var _level = 0;
+$(function () {
     $.ajax({
         url: './ListResult',
         type: 'post',
         data: {},
         dataType: 'json',
         success: function (result) {
-            loadNav(result.data);
+            _navList = result.data;
+            var childList = _navList.filter(a => a.parentId == 0);
+            var html = loadNav(childList);
+            $('.nav-list').html(html);
         }
     });
 });
 
 function loadNav(result) {
+    if (result.length <= 0) {
+        _level = 0;
+        return '';
+    }
     var _html = '';
     $.each(result, function (i, item) {
-        _html += `<tr>
-                            <td class="border-0"><i class="fas fa-caret-right fa-fw"></i>${item.name}</td>
-                        </tr>`;
+        var childList = _navList.filter(a => a.parentId == item.id);
+        _html += `<li class="nav-item">`;
+        _html += `<a class="nav-link">`;
+        if (childList.length > 0) {
+            _html += `     <i class="fas fa-caret-right fa-fw"></i>`;
+        } else {
+            _html += `${getSpaceStr()}`;
+        }
+        _html += `${item.name}<button type="button" class="btn btn-primary float-right">Primary</button>`;
+        _html += '</a>';
+        _html += `</li>`;
+        if (childList.length > 0) {
+            _html += `<li class="nav-item">`;
+            _html += `<ul class="nav nav-pills flex-column nav-list">`;
+            _html += `     ${loadNav(childList)}`;
+            _html += `</ul>`;
+            _html += `</li>`;
+        }
     });
-    $('.nav-list').html(_html);
+    _level++;
+    return _html;
+}
+
+function getSpaceStr() {
+    var str = '';
+    for (var i = 0; i < _level; i++) {
+        str += '&nbsp;&nbsp;';
+    }
+    return str;
 }
