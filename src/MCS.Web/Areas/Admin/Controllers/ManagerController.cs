7f6b03749ffa4062ecf496aeaca7ae533c3313d7
MiZone <mizonerom@hotmail.com>
2020-12-08 17:30:23 +0800

    -

---
+++ src/MCS.Web/Areas/Admin/Controllers/ManagerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MCS.Web.Framework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCS.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManagerController : BaseAdminController
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MCS.Web/Areas/Admin/Controllers/PrivilegeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCS.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PrivilegeController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
