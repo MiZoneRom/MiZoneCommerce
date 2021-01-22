05c4dd1d30d2d35a39174a71a7ff0f332dcf65c2
MiZone <mizonerom@hotmail.com>
2021-01-22 16:54:34 +0800

    -

---
+++ src/MCS.AdminAPI/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using MCS.Web.Framework.BaseControllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCS.AdminAPI.Controllers
+{
+    [ApiController]
+    [Route("api/admin/[controller]")]
+    public class LoginController : BaseAPIController
+    {
+    }
+}
