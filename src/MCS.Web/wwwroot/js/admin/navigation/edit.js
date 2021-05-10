65a33246b41412b783dff6c6383ded706a7b123a
MiZone <mizonerom@hotmail.com>
2021-05-10 15:51:34 +0800

    -

---
+++ src/MCS.Web/wwwroot/js/admin/navigation/edit.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('.btn-add-action').click(function () {
+        var html = `                                    <tr>
+                                        <th>名称</th>
+                                        <th>代号</th>
+                                        <th>操作</th>
+                                    </tr>`;
+        $('.table-actions tbody').append(html);
+    });
+});
