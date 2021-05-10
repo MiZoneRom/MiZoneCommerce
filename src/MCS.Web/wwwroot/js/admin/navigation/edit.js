eaba259cac80bc5b0d2f0996cffa3d25cfe856db
MiZone <mizonerom@hotmail.com>
2021-05-10 16:13:04 +0800

    -

--- src/MCS.Web/wwwroot/js/admin/navigation/edit.js
+++ src/MCS.Web/wwwroot/js/admin/navigation/edit.js
@@ -1,10 +1,14 @@
 ﻿$(function () {
+    $('.table-actions').on('click', '.btn-delete', function () {
+        $(this).parents('tr').remove();
+    });
+
     $('.btn-add-action').click(function () {
         var html = `
                     <tr>
                         <th><input class="form-control" name="Name" type="text" value="" readonly="readonly" /></th>
                         <th><input class="form-control" name="AccessKey" type="text" value="" readonly="readonly" /></th>
-                        <th><input class="form-control" name="Id" type="hidden" value="0" /> <input class="btn btn-danger" type="button" value="删除" /></th>
+                        <th><input class="form-control" name="Id" type="hidden" value="0" /> <input class="btn btn-danger btn-delete" type="button" value="删除" /></th>
                     </tr>`;
         $('.table-actions tbody').append(html);
     });
